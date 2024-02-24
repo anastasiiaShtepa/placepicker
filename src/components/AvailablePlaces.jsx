55c55f3f6af5c643c7078e8c8e8f0c4b451286da
Anastasiia <anastasiia.shtepa1990@gmail.com>
2024-02-24 15:52:45 +0100

    fetched data from backend

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,21 @@
-import Places from './Places.jsx';
+import { useState, useEffect } from "react";
+import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availableplaces, setAvailablePlaces] = useState([]);
+  useEffect(() => {
+    async function fetchPlaces() {
+      const response = await fetch("http://localhost:81/places");
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+    }
+    fetchPlaces()
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availableplaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
