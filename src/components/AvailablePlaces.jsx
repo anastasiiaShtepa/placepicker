ebd5e2d941253dcee9b5114e61d73b16b3c70b3a
Anastasiia <anastasiia.shtepa1990@gmail.com>
2024-02-25 09:17:14 +0100

    added geolocation

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,20 +1,37 @@
 import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
+import { sortPlacesByDistance } from "../utils/loc.js";
+import { fetchAvailablePlaces } from "../utils/http.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
   const [availableplaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true);
-      const response = await fetch("http://localhost:81/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
-      setIsFetching(false);
+      try {
+        const places = await fetchAvailablePlaces();
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
+      } catch (error) {
+        setError({ message: error.message || "Could not fetch places" });
+        setIsFetching(false);
+      }
     }
     fetchPlaces();
   }, []);
-
+  if (error) {
+    return <div>Error</div>;
+  }
   return (
     <Places
       title="Available Places"
